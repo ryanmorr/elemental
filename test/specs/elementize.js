7341795782f3fecaddea5aa8231053bf7cd84038
Ryan Morr <rm.morr@gmail.com>
2023-07-24 12:26:10 -0400

    Improve managing of test element tag names

--- test/specs/elementize.js
+++ test/specs/elementize.js
@@ -1,47 +1,51 @@
 import elementize from '../../src/elementize';
 
 describe('elementize', () => {
+    let index = 0;
+    const generateTagName = () => `test-element-${++index}`;
+    const getTagName = () => `test-element-${index}`;
+
     it('should create and register a custom element', () => {
         const callback = sinon.spy(function() {
             return document.createTextNode('foo');
         });
 
-        const CustomElement1 = elementize('test-element1', callback);
+        const CustomElement = elementize(generateTagName(), callback);
 
-        expect(CustomElement1).to.be.a('function');
+        expect(CustomElement).to.be.a('function');
         expect(callback.callCount).to.equal(0);
 
-        const constructor = customElements.get('test-element1');
+        const constructor = customElements.get(getTagName());
         expect(constructor).to.exist;
-        expect(constructor).to.equal(CustomElement1);
+        expect(constructor).to.equal(CustomElement);
 
-        const element = document.createElement('test-element1');
+        const element = document.createElement(getTagName());
 
         expect(callback.callCount).to.equal(1);
         expect(callback.args[0][0]).to.equal(element);
         expect(callback.calledOn(element)).to.equal(true);
 
         expect(element.nodeType).to.equal(1);
-        expect(element.tagName).to.equal('TEST-ELEMENT1');
+        expect(element.localName).to.equal(getTagName());
         expect(element.shadowRoot.textContent).to.equal('foo');
-        expect(element).to.be.an.instanceof(CustomElement1);
+        expect(element).to.be.an.instanceof(CustomElement);
         expect(element).to.be.an.instanceof(HTMLElement);
     });
 
     it('should support shadow content as an HTML string', () => {
-        elementize('test-element2', () => {
+        elementize(generateTagName(), () => {
             return '<div>foo</div>';
         });
 
-        const element = document.createElement('test-element2');
+        const element = document.createElement(getTagName());
         expect(element.shadowRoot.innerHTML).to.equal('<div>foo</div>');
     });
 
     it('should support not returning shadow content', () => {
         const spy = sinon.spy();
-        elementize('test-element3', spy);
+        elementize(generateTagName(), spy);
 
-        const element = document.createElement('test-element3');
+        const element = document.createElement(getTagName());
         expect(element.shadowRoot.innerHTML).to.equal('');
     });
 });
